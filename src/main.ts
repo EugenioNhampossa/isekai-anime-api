aa0c8c4e6a89051746ed83d122699c7343c6d1b7
EugenioNhampossa <eugenioanhampossa@gmail.com>
2023-07-10 23:31:44 +0200

    chore: add helmet and fastify

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import helmet from '@fastify/helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true }),
+  );
+  await app.register(helmet);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
+      enableDebugMessages: true,
     }),
   );
-  await app.listen(process.env.PORT || 3333);
+  app.enableCors();
+  await app.listen(process.env.PORT || 3333, '0.0.0.0');
 }
 bootstrap();
